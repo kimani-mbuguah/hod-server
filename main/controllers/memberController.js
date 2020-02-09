10dbbec99722199c0fb91b9c35b7dd5f0ba89559
kim <kimanimbuguah@gmail.com>
2020-02-09 19:20:24 +0300

    streamling member management and sms sending

--- main/controllers/memberController.js
+++ main/controllers/memberController.js
@@ -1,22 +1,22 @@
-const AfricasTalking = require("africastalking");
+const AfricasTalking = require('africastalking')
 
-const Member = require("mongoose").model("Member");
-const Relationship = require("mongoose").model("Relationship");
-const SmsGroup = require("mongoose").model("SmsGroup");
-const { validations } = require("../../config");
+const Member = require('mongoose').model('Member')
+const Relationship = require('mongoose').model('Relationship')
+const SmsGroup = require('mongoose').model('SmsGroup')
+const { validations } = require('../../config')
 
 const africasTalking = new AfricasTalking({
-  username: "pesabase-admin",
-  apiKey: "93d85428978f6806b5de86dae1a903e45824d710bf59b71d71b40bf27cfec37f"
-});
+  username: 'pesabase-admin',
+  apiKey: '93d85428978f6806b5de86dae1a903e45824d710bf59b71d71b40bf27cfec37f'
+})
 
-const sms = africasTalking.SMS;
+const sms = africasTalking.SMS
 
 // POST /member/register
 exports.postRegMember = (req, res, next) => {
-  const validationResult = validateMemberRegForm(req.body);
+  const validationResult = validateMemberRegForm(req.body)
   if (!validationResult.success) {
-    return res.status(400).json(validationResult.errors);
+    return res.status(400).json(validationResult.errors)
   }
 
   const memberData = {
@@ -28,25 +28,25 @@ exports.postRegMember = (req, res, next) => {
     email: req.body.email.trim(),
     city: req.body.city.trim(),
     county: req.body.county.trim()
-  };
-  const newMember = new Member(memberData);
+  }
+  const newMember = new Member(memberData)
   newMember
     .save()
     .then(member => {
-      return res.status(200).json(member);
+      return res.status(200).json(member)
     })
     .catch(error => {
-      if (error.name === "MongoError" && error.code === 11000) {
+      if (error.name === 'MongoError' && error.code === 11000) {
         return res.status(409).json({
-          email: "Conflicting email found",
-          phoneNumber: "Conflicting Phone number found"
-        });
+          email: 'Conflicting email found',
+          phoneNumber: 'Conflicting Phone number found'
+        })
       }
       return res.status(400).json({
         error: error
-      });
-    });
-};
+      })
+    })
+}
 
 exports.postUpdateMemberProfile = (req, res) => {
   Member.findOne({ _id: req.body.id }).then(member => {
@@ -69,20 +69,20 @@ exports.postUpdateMemberProfile = (req, res) => {
         })
         .then(() => {
           return res.status(200).json({
-            message: "Profile updated successfully"
-          });
+            message: 'Profile updated successfully'
+          })
         })
         .catch(err => {
           return res.status(400).json({
-            message: "An error occurred while updating profile"
-          });
-        });
+            message: 'An error occurred while updating profile'
+          })
+        })
     }
-  });
-};
+  })
+}
 
 exports.postUpdateMemberProfilePic = (req, res) => {
-  console.log(req.body);
+  console.log(req.body)
   Member.findOne({ _id: req.body.id }).then(member => {
     if (member) {
       member
@@ -91,212 +91,211 @@ exports.postUpdateMemberProfilePic = (req, res) => {
         })
         .then(() => {
           return res.status(200).json({
-            message: "Profile Pic updated successfully"
-          });
+            message: 'Profile Pic updated successfully'
+          })
         })
         .catch(err => {
           return res.status(400).json({
-            message: "An error occurred while updating profile pic"
-          });
-        });
+            message: 'An error occurred while updating profile pic'
+          })
+        })
     }
-  });
-};
+  })
+}
 // GET /member/list
 // List members, paginations options
 exports.list = function(req, res, next) {
   const pageOptions = {
-    page: req.query["page"] || 1,
-    limit: req.query["limit"] || 1000,
-    sort: req.query["sort"] || "name asc",
+    page: req.query['page'] || 1,
+    limit: req.query['limit'] || 1000,
+    sort: req.query['sort'] || 'name asc',
     populate: {
-      path: "relationships",
-      select: "fullName relationship",
-      model: "Relationship"
+      path: 'relationships',
+      select: 'fullName relationship',
+      model: 'Relationship'
     }
-  };
+  }
 
-  let filterOptions = {};
+  let filterOptions = {}
   try {
-    const filterParam = req.query["filter"];
+    const filterParam = req.query['filter']
     if (Array.isArray(filterParam) && filterParam.length > 0) {
       filterParam.forEach(item => {
-        filterOptions[item.id] = new RegExp(item.value, "i");
-      });
+        filterOptions[item.id] = new RegExp(item.value, 'i')
+      })
     }
   } catch (err) {
-    console.log("Could not parse 'filter' param " + err);
+    console.log("Could not parse 'filter' param " + err)
   }
 
   Member.paginate(filterOptions, pageOptions, (err, result) => {
     if (err) {
-      console.log(err);
+      console.log(err)
       return res.status(500).json({
         success: false,
         errors: [JSON.stringify(err)]
-      });
+      })
     }
-    return res.json(result.docs);
-  });
-};
+    return res.json(result.docs)
+  })
+}
 
 exports.countMembers = (req, res) => {
   Member.countDocuments({}, function(err, count) {
-    console.log("Number of docs: ", count);
+    console.log('Number of docs: ', count)
     return res.status(200).json({
       count: count
-    });
+    })
   }).catch(err => {
-    return res.status(400).json(err);
-  });
-};
+    return res.status(400).json(err)
+  })
+}
 
 // GET /member/list/:email
 exports.find = (req, res) => {
   Member.findOne({ email: req.params.email })
     .populate({
-      path: "relationships successfulReferrals",
-      select: "fullName relationship",
-      model: "Relationship"
+      path: 'relationships successfulReferrals',
+      select: 'fullName relationship',
+      model: 'Relationship'
     })
     .then(member => {
       if (!member) {
         return res.status(404).json({
           email: `No such member in the records`
-        });
+        })
       }
 
       return res.json({
         member
-      });
-    });
-};
+      })
+    })
+}
 
 // GET /member/sms
 // List members, paginations options
 exports.membersms = (req, res, next) => {
   const pageOptions = {
-    page: req.query["page"] || 1,
-    limit: req.query["limit"] || 1000,
-    sort: req.query["sort"] || "name asc"
-  };
+    page: req.query['page'] || 1,
+    limit: req.query['limit'] || 1000,
+    sort: req.query['sort'] || 'name asc'
+  }
 
-  let filterOptions = {};
+  let filterOptions = {}
   try {
-    const filterParam = req.query["filter"];
+    const filterParam = req.query['filter']
     if (Array.isArray(filterParam) && filterParam.length > 0) {
       filterParam.forEach(item => {
-        filterOptions[item.id] = new RegExp(item.value, "i");
-      });
+        filterOptions[item.id] = new RegExp(item.value, 'i')
+      })
     }
   } catch (err) {
-    console.log("Could not parse 'filter' param " + err);
+    console.log("Could not parse 'filter' param " + err)
   }
 
   Member.paginate(filterOptions, pageOptions, (err, result) => {
     if (err) {
-      console.log(err);
+      console.log(err)
       return res.status(500).json({
         success: false,
         errors: [JSON.stringify(err)]
-      });
+      })
     }
 
-    const users = result.docs;
+    const users = result.docs
 
-    smsList = [];
+    smsList = []
     for (var i = 0; i < users.length; i++) {
-      user = users[i];
+      user = users[i]
       smsList.push({
         name: user.fullName,
         value: user.phoneNumber
-      });
+      })
     }
-    return res.json(smsList);
-  });
-};
+    return res.json(smsList)
+  })
+}
 
 // GET /member/listrelationship
 exports.listRelationshipMembers = (req, res, next) => {
   const pageOptions = {
-    page: req.query["page"] || 1,
-    limit: req.query["limit"] || 1000,
-    sort: req.query["sort"] || "name asc"
-  };
+    page: req.query['page'] || 1,
+    limit: req.query['limit'] || 1000,
+    sort: req.query['sort'] || 'name asc'
+  }
 
-  let filterOptions = {};
+  let filterOptions = {}
   try {
-    const filterParam = req.query["filter"];
+    const filterParam = req.query['filter']
     if (Array.isArray(filterParam) && filterParam.length > 0) {
       filterParam.forEach(item => {
-        filterOptions[item.id] = new RegExp(item.value, "i");
-      });
+        filterOptions[item.id] = new RegExp(item.value, 'i')
+      })
     }
   } catch (err) {
-    console.log("Could not parse 'filter' param " + err);
+    console.log("Could not parse 'filter' param " + err)
   }
 
   Member.paginate(filterOptions, pageOptions, (err, result) => {
     if (err) {
-      console.log(err);
+      console.log(err)
       return res.status(500).json({
         success: false,
         errors: [JSON.stringify(err)]
-      });
+      })
     }
 
-    const users = result.docs;
+    const users = result.docs
 
-    relationshipList = [];
+    relationshipList = []
     for (var i = 0; i < users.length; i++) {
-      user = users[i];
+      user = users[i]
       relationshipList.push({
         name: user.fullName,
         value: user._id
-      });
+      })
     }
-    return res.json(relationshipList);
-  });
-};
+    return res.json(relationshipList)
+  })
+}
 
 exports.addRelationship = (req, res) => {
-  const payload = req.body;
+  const payload = req.body
   Member.findOne({ _id: payload.member }).then(member => {
     Member.findOne({ _id: payload.relatedTo })
       .then(relatedTo => {
         const newRelationship = new Relationship({
-          fullName: member.fullName,
+          fullName: relatedTo.fullName,
           relationship: payload.relationship
-        });
+        })
 
         newRelationship
           .save()
           .then(relationship => {
-            console.log(relationship);
-            member.relationships.push(relationship);
+            member.relationships.push(relationship)
             member
               .save()
               .then(() => {
-                console.log("new relationship added");
+                console.log('new relationship added')
                 return res.status(200).json({
-                  message: "Relationship added successfully"
-                });
+                  message: 'Relationship added successfully'
+                })
               })
               .catch(error => {
-                console.log(error);
-                return res.status(400).json(error);
-              });
+                console.log(error)
+                return res.status(400).json(error)
+              })
           })
           .catch(error => {
-            console.log(error);
-            return res.status(400).json(error);
-          });
+            console.log(error)
+            return res.status(400).json(error)
+          })
       })
       .catch(err => {
-        return res.status(400).json(error);
-      });
-  });
-};
+        return res.status(400).json(error)
+      })
+  })
+}
 
 exports.sendSms = (req, res, next) => {
   sms
@@ -306,20 +305,20 @@ exports.sendSms = (req, res, next) => {
     })
     .then(function(response) {
       res.status(200).json({
-        message: "SMS sent"
-      });
+        message: 'SMS sent'
+      })
     })
     .catch(error => {
       res.status(400).json({
-        message: "SMS not sent"
-      });
-    });
-};
+        message: 'SMS not sent'
+      })
+    })
+}
 
 exports.multipleSmSRecipients = (req, res) => {
-  const smsRecipients = req.body.smsRecipients;
-  const message = req.body.message;
-  console.log(req.body);
+  const smsRecipients = req.body.smsRecipients
+  const message = req.body.message
+  console.log(req.body)
   for (let recipient in smsRecipients) {
     sms
       .send({
@@ -327,46 +326,68 @@ exports.multipleSmSRecipients = (req, res) => {
         message: message
       })
       .then(response => {
-        console.log("sms sent");
+        console.log('sms sent')
       })
       .catch(error => {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
   res.status(200).json({
-    message: "Sending sms"
-  });
-};
+    message: 'Sending sms'
+  })
+}
 
-exports.createCustomGroup = (req, res) => {
+exports.getCustomGroups = (req, res) => {
+  SmsGroup.find({})
+    .then(groups => {
+      return res.status(200).json(groups)
+    })
+    .catch(err => {
+      return res.status(400).json({
+        error: err
+      })
+    })
+}
+
+exports.createCustomGroup = async (req, res) => {
   if (!req.body.members || !req.body.name) {
     return res.status(400).json({
       success: false,
-      message: "One or more required fields is missing"
-    });
+      message: 'One or more required fields is missing'
+    })
   }
-  const newSmsGroup = new SmsGroup({
-    name: req.body.name,
-    members: req.body.members
-  });
 
-  newSmsGroup
-    .save()
-    .then(smsgroup => {
-      console.log(smsgroup);
-      return res.status(200).json({
-        message: "Group created successfully"
-      });
+  await createMembers(req.body.members)
+    .then(groupMembers => {
+      const newSmsGroup = new SmsGroup({
+        name: req.body.name,
+        members: groupMembers
+      })
+
+      newSmsGroup
+        .save()
+        .then(() => {
+          return res.status(200).json({
+            message: 'Group created successfully'
+          })
+        })
+        .catch(err => {
+          return res.status(400).json({
+            error: err
+          })
+        })
     })
     .catch(err => {
-      return res.status(400).json(err);
-    });
-};
+      return res.status(400).json({
+        error: err
+      })
+    })
+}
 
 exports.sendGroupSMS = (req, res) => {
-  const group = req.body.group;
-  const message = req.body.message;
+  const group = req.body.group
+  const message = req.body.message
 
   Member.find({ ministry: group }).then(members => {
     if (members) {
@@ -377,111 +398,164 @@ exports.sendGroupSMS = (req, res) => {
             message: message
           })
           .then(response => {
-            console.log("sms sent");
+            console.log('sms sent')
           })
           .catch(error => {
-            console.log(error);
-          });
+            console.log(error)
+          })
       }
     }
     res.status(200).json({
-      message: "Sending sms"
-    });
-  });
-};
+      message: 'Sending sms'
+    })
+  })
+}
+
+exports.sendCustomGroupSMS = (req, res) => {
+  const group = req.body.group
+  const message = req.body.message
+
+  console.log(req.body)
+
+  SmsGroup.findOne({ name: group }).then(group => {
+    const members = group.members
+
+    for (let i = 0; i < members.length; i++) {
+      sms
+        .send({
+          to: members[i].phoneNumber,
+          message: message
+        })
+        .then(response => {
+          console.log('sms sent')
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  })
+
+  res.status(200).json({
+    message: 'Sending sms'
+  })
+}
 
 const validateMemberRegForm = payload => {
-  const errors = {};
-  let isFormValid = true;
-  let message = "";
+  const errors = {}
+  let isFormValid = true
+  let message = ''
 
   if (
     !payload ||
-    typeof payload.email !== "string" ||
+    typeof payload.email !== 'string' ||
     !validations.email.regex.value.test(payload.email.trim())
   ) {
-    isFormValid = false;
-    errors.email = validations.email.regex.message;
-    errors.isEmailError = true;
+    isFormValid = false
+    errors.email = validations.email.regex.message
+    errors.isEmailError = true
   }
 
   if (
     !payload ||
-    typeof payload.fullName !== "string" ||
+    typeof payload.fullName !== 'string' ||
     payload.fullName.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.fullName = "Member's name is required";
-    errors.isFullNameError = true;
+    isFormValid = false
+    errors.fullName = "Member's name is required"
+    errors.isFullNameError = true
   }
 
   if (
     !payload ||
-    typeof payload.phoneNumber !== "string" ||
+    typeof payload.phoneNumber !== 'string' ||
     payload.phoneNumber.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.phoneNumber = "Phone number is required";
-    errors.isPhoneNumberError = true;
+    isFormValid = false
+    errors.phoneNumber = 'Phone number is required'
+    errors.isPhoneNumberError = true
   }
 
   if (
     !payload ||
-    typeof payload.occupation !== "string" ||
+    typeof payload.occupation !== 'string' ||
     payload.occupation.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.occupation = "Occupation is required";
-    errors.isOccupationError = true;
+    isFormValid = false
+    errors.occupation = 'Occupation is required'
+    errors.isOccupationError = true
   }
 
   if (
     !payload ||
-    typeof payload.ministry !== "string" ||
+    typeof payload.ministry !== 'string' ||
     payload.ministry.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.ministry = "Ministry is required";
-    errors.isMinistryError = true;
+    isFormValid = false
+    errors.ministry = 'Ministry is required'
+    errors.isMinistryError = true
   }
 
   if (
     !payload ||
-    typeof payload.city !== "string" ||
+    typeof payload.city !== 'string' ||
     payload.city.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.city = "Town/city of residence is required";
-    errors.isCityError = true;
+    isFormValid = false
+    errors.city = 'Town/city of residence is required'
+    errors.isCityError = true
   }
 
   if (
     !payload ||
-    typeof payload.county !== "string" ||
+    typeof payload.county !== 'string' ||
     payload.county.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.county = "County of residence is required";
-    errors.isCountyError = true;
+    isFormValid = false
+    errors.county = 'County of residence is required'
+    errors.isCountyError = true
   }
 
   if (
     !payload ||
-    typeof payload.about !== "string" ||
+    typeof payload.about !== 'string' ||
     payload.about.trim().length === 0
   ) {
-    isFormValid = false;
-    errors.about = "Member's Bio is required";
-    errors.isAboutError = true;
+    isFormValid = false
+    errors.about = "Member's Bio is required"
+    errors.isAboutError = true
   }
 
   if (!isFormValid) {
-    message = "Check the form for errors.";
+    message = 'Check the form for errors.'
   }
 
   return {
     success: isFormValid,
     message,
     errors
-  };
-};
+  }
+}
+
+const createMembers = async phoneNumbers => {
+  return new Promise(async (resolve, reject) => {
+    const groupMembers = []
+    for (let i = 0; i < phoneNumbers.length; i++) {
+      await Member.findOne({ phoneNumber: phoneNumbers[i] })
+        .then(member => {
+          groupMembers.push({
+            name: member.fullName,
+            phoneNumber: member.phoneNumber
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+
+    if (groupMembers.length > 0) {
+      resolve(groupMembers)
+    } else {
+      reject('no members')
+    }
+  })
+}
--- routes/member.js
+++ routes/member.js
@@ -1,98 +1,112 @@
-const express = require("express");
-const router = express.Router();
-const authCheck = require("../middleware/auth-check");
-const Roles = require("../validation/roles");
-const memberController = require("../main/controllers/memberController");
+const express = require('express')
+const router = express.Router()
+const authCheck = require('../middleware/auth-check')
+const Roles = require('../validation/roles')
+const memberController = require('../main/controllers/memberController')
 
 // POST /member/register
 router.post(
-  "/register",
+  '/register',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.postRegMember
-);
+)
 
 //GET /member/list
 router.get(
-  "/list",
+  '/list',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.list
-);
+)
 
 //GET /member/list/:email
 router.get(
-  "/list/:email",
+  '/list/:email',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.find
-);
+)
 
 //GET /member/list
 router.get(
-  "/listsms",
+  '/listsms',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.membersms
-);
+)
 
 //GET /member/listrelationship
 router.get(
-  "/listrelationship",
+  '/listrelationship',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.listRelationshipMembers
-);
+)
 
 //POST /member/addrelationship
 router.post(
-  "/addrelationship",
+  '/addrelationship',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.addRelationship
-);
+)
 
 //POST /member/updateprofile
 router.post(
-  "/updateprofile",
+  '/updateprofile',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.postUpdateMemberProfile
-);
+)
 
 //POST /member/updateprofilepic
 router.post(
-  "/updateprofilepic",
+  '/updateprofilepic',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.postUpdateMemberProfilePic
-);
+)
 
 //POST /member/sendsms
 router.post(
-  "/sendsms",
+  '/sendsms',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.sendSms
-);
+)
 
 //POST /member/multiplesmsrecipients
 router.post(
-  "/multiplesmsrecipients",
+  '/multiplesmsrecipients',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.multipleSmSRecipients
-);
+)
 
 //POST /member/creategroup
 router.post(
-  "/creategroup",
+  '/creategroup',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.createCustomGroup
-);
+)
 
 //POST /member/groupsms
 router.post(
-  "/groupsms",
+  '/groupsms',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.sendGroupSMS
-);
+)
+
+//POST /member/customgroupsms
+router.post(
+  '/customgroupsms',
+  authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
+  memberController.sendCustomGroupSMS
+)
 
 //GET /member/count
 router.get(
-  "/count",
+  '/count',
   authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
   memberController.countMembers
-);
+)
 
-module.exports = router;
+//GET /member/getgroups
+router.get(
+  '/getgroups',
+  authCheck([Roles.member, Roles.admin, Roles.superAdmin]),
+  memberController.getCustomGroups
+)
+
+module.exports = router
